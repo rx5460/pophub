b87506dcc7d101302b50319bebf81b0028460d06
rx5460 <rx460@naver.com>
2024-05-18 15:03:55 +0900

    change : 결제 테스트 페이지 추가 change : pubspec.yaml에 webview_flutter 추가

---
+++ lib/screen/user/purchase_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PurchasePage extends StatefulWidget {
+  const PurchasePage({super.key});
+
+  @override
+  State<PurchasePage> createState() => _PurchasePageState();
+}
+
+class _PurchasePageState extends State<PurchasePage> {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  late final WebViewController _webViewController;
+  String token = "";
+  String profileData = "";
+
+  Future<void> _showToken() async {
+    token = (await _storage.read(key: 'token'))!;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _showToken();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) async {
+            /// 황지민 : 결제시 intent 버그를 막기위한 작업
+            if (request.url.startsWith('intent')) {
+              return NavigationDecision.prevent;
+            }
+
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(
+          'https://online-pay.kakao.com/mockup/v1/ecc24646dccedae14bd1235b66dc40cfe0a4198128b6371fdb3e594a1e70d8b5/mInfo'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: WebViewWidget(controller: _webViewController),
+    ));
+  }
+}
