8306d3bc62f23c3ac24e125053e0a6313af47f5f
rx5460 <rx460@naver.com>
2024-06-09 15:47:13 +0900

    - change : 프로필 수정 디자인 변경

--- lib/screen/user/acount_info.dart
+++ lib/screen/user/acount_info.dart
@@ -2,6 +2,7 @@ import 'dart:io' show File;
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:pophub/assets/constants.dart';
 import 'package:pophub/model/user.dart';
 import 'package:pophub/notifier/UserNotifier.dart';
 import 'package:pophub/screen/custom/custom_title_bar.dart';
@@ -25,6 +26,11 @@ class _AcountInfoState extends State<AcountInfo> {
 
   final ImagePicker _picker = ImagePicker();
   XFile? _image;
+  @override
+  void initState() {
+    super.initState();
+    nicknameController.text = User().userName;
+  }
 
   Future<void> _pickImage() async {
     try {
@@ -91,59 +97,111 @@ class _AcountInfoState extends State<AcountInfo> {
     Size screenSize = MediaQuery.of(context).size;
     double screenWidth = screenSize.width;
     double screenHeight = screenSize.height;
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: const CustomTitleBar(
         titleName: "계정 정보",
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 8,
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: screenWidth * 0.4,
+                          height: screenHeight * 0.2,
+                          child: CircleAvatar(
+                            backgroundImage: _image == null
+                                ? User().file != ""
+                                    ? FileImage(File(User().file))
+                                    : const AssetImage('assets/images/logo.png')
+                                        as ImageProvider
+                                : FileImage(File(_image!.path))
+                                    as ImageProvider,
+                            radius: 1000,
+                          ),
+                        ),
+                        Transform.translate(
+                          offset: Offset(screenWidth * 0.2 - 18, -48),
+                          child: GestureDetector(
+                            onTap: () {
+                              _pickImage();
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 1,
+                                    color: const Color(0xFFADD8E6),
+                                  ),
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(50)),
+                                  color: Colors.white),
+                              child: const Padding(
+                                padding: EdgeInsets.all(4.0),
+                                child: Icon(
+                                  Icons.settings_outlined,
+                                  color: Colors.black,
+                                  size: 24,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                SizedBox(
+                  width: screenWidth * 0.9,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                        width: screenWidth * 0.4,
-                        height: screenHeight * 0.2,
-                        child: CircleAvatar(
-                          backgroundImage: _image == null
-                              ? User().file != ""
-                                  ? FileImage(File(User().file))
-                                  : const AssetImage('assets/images/logo.png')
-                                      as ImageProvider
-                              : FileImage(File(_image!.path)) as ImageProvider,
-                          radius: 1000,
+                      Expanded(
+                        child: TextField(
+                          controller: nicknameController,
+                          onChanged: (value) {
+                            setState(() {
+                              checked = false;
+                              nicknameInput = value;
+                            });
+                          },
+                          readOnly: checked,
+                          decoration: InputDecoration(
+                            labelText: User().userName,
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                          ),
                         ),
                       ),
-                      Transform.translate(
-                        offset: Offset(screenWidth * 0.2 - 18, -48),
-                        child: GestureDetector(
-                          onTap: () {
-                            _pickImage();
+                      SizedBox(
+                        width: screenWidth * 0.1,
+                      ),
+                      SizedBox(
+                        width: screenWidth * 0.22,
+                        height: screenHeight * 0.065,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            if (checked) {
+                              setState(() {
+                                checked = false;
+                              });
+                            } else if (nicknameInput != '') {
+                              nameCheckApi();
+                            }
                           },
-                          child: Container(
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                  width: 1,
-                                  color: const Color(0xFFADD8E6),
-                                ),
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(50)),
-                                color: Colors.white),
-                            child: const Padding(
-                              padding: EdgeInsets.all(4.0),
-                              child: Icon(
-                                Icons.settings_outlined,
-                                color: Colors.black,
-                                size: 24,
+                          child: Center(
+                            child: Text(
+                              checked ? '수정' : '중복확인',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
                               ),
                             ),
                           ),
@@ -152,191 +210,124 @@ class _AcountInfoState extends State<AcountInfo> {
                     ],
                   ),
                 ),
-              ),
-              SizedBox(
-                width: screenWidth * 0.85,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: screenWidth * 0.6,
-                      height: screenHeight * 0.07,
-                      child: TextField(
-                        controller: nicknameController,
-                        onChanged: (value) {
-                          setState(() {
-                            checked = false;
-                            nicknameInput = value;
-                          });
-                        },
-                        readOnly: checked,
-                        decoration: InputDecoration(
-                          labelText: User().userName,
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: screenWidth * 0.22,
-                      height: screenHeight * 0.07,
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        border: Border.all(
-                          width: 2,
-                          color: const Color(0xFFE6A3B3),
-                        ),
-                        // color: const Color(0xFFE6A3B3)
-                      ),
-                      child: InkWell(
-                        onTap: () {
-                          if (checked) {
-                            setState(() {
-                              checked = false;
-                            });
-                          } else if (nicknameInput != '') {
-                            nameCheckApi();
-                          }
-                        },
-                        child: Center(
-                          child: Text(
-                            checked ? '수정' : '중복확인',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                SizedBox(
+                  height: screenHeight * 0.03,
                 ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.03,
-              ),
-              Container(
-                width: screenWidth * 0.85,
-                height: screenHeight * 0.07,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  border: Border.all(
-                    width: 2,
-                    color: const Color(0xFFADD8E6),
-                  ),
-                  // color: const Color(0xFFE6A3B3)
-                ),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MultiProvider(providers: [
-                                  ChangeNotifierProvider(
-                                      create: (_) => UserNotifier())
-                                ], child: const ResetPasswd())));
-                  },
-                  child: const Center(
-                    child: Text(
-                      '비밀번호 재설정',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.03,
-              ),
-              Visibility(
-                visible: User().role == "General Memeber",
-                child: Container(
-                  width: screenWidth * 0.85,
+                SizedBox(
+                  width: screenWidth * 0.9,
                   height: screenHeight * 0.07,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    border: Border.all(
-                      width: 2,
-                      color: const Color(0xFFADD8E6),
-                    ),
-                    // color: const Color(0xFFE6A3B3)
-                  ),
-                  child: Center(
-                    child: Text(
-                      '성별 : ${User().gender}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black,
-                        fontSize: 16,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MultiProvider(providers: [
+                                    ChangeNotifierProvider(
+                                        create: (_) => UserNotifier())
+                                  ], child: const ResetPasswd())));
+                    },
+                    child: const Center(
+                      child: Text(
+                        '비밀번호 재설정',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.03,
-              ),
-              Visibility(
-                visible: User().role == "General Memeber",
-                child: Container(
-                  width: screenWidth * 0.85,
-                  height: screenHeight * 0.07,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    border: Border.all(
-                      width: 2,
-                      color: const Color(0xFFADD8E6),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Visibility(
+                  visible: User().role == "General Member",
+                  child: Container(
+                    width: screenWidth * 0.9,
+                    height: screenHeight * 0.07,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      border: Border.all(
+                        width: 1,
+                        color: Constants.BUTTON_GREY,
+                      ),
                     ),
-                    // color: const Color(0xFFE6A3B3)
-                  ),
-                  child: Center(
-                    child: Text(
-                      '나이 : ${User().age}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black,
-                        fontSize: 16,
+                    child: Center(
+                      child: Text(
+                        '성별 : ${User().gender}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Visibility(
+                  visible: User().role == "General Member",
+                  child: Container(
+                    width: screenWidth * 0.9,
+                    height: screenHeight * 0.07,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      border: Border.all(
+                        width: 1,
+                        color: Constants.BUTTON_GREY,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '나이 : ${User().age}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.1), // Added space for button
+              ],
+            ),
           ),
-          InkWell(
-            onTap: () {
-              if (checked) {
-                if (_image == null) {
-                  profileModify();
-                } else {
-                  profileModifyImage();
-                }
-              } else {
-                showAlert(context, "경고", "닉네임 중복확인을 해주세요.", () {
-                  Navigator.of(context).pop();
-                });
-              }
-            },
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              width: screenWidth,
-              height: screenHeight * 0.1,
-              color: const Color(0xFFADD8E6),
-              child: const Center(
-                child: Text(
-                  '수정하기',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.white,
+              width: screenWidth * 0.9,
+              height: screenHeight * 0.07,
+              margin: const EdgeInsets.only(bottom: 15),
+              child: OutlinedButton(
+                onPressed: () {
+                  if (checked) {
+                    if (_image == null) {
+                      profileModify();
+                    } else {
+                      profileModifyImage();
+                    }
+                  } else {
+                    showAlert(context, "경고", "닉네임 중복확인을 해주세요.", () {
+                      Navigator.of(context).pop();
+                    });
+                  }
+                },
+                child: const Center(
+                  child: Text(
+                    '수정하기',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
