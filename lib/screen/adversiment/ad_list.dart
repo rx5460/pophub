9d4126eda525d7aaee9429303b357eb1dfbd994d
Promark1225 <wsw7854@naver.com>
2024-09-09 14:03:54 +0900

    fix: AdRequestDetail

--- lib/screen/adversiment/ad_list.dart
+++ lib/screen/adversiment/ad_list.dart
@@ -102,6 +102,9 @@ class AdListState extends State<AdList> with SingleTickerProviderStateMixin {
           date: '2024.09.06',
           status: '답변하기',
           isCompleted: false,
+          content: '광고에 대해 문의 드립니다. 더 많은 정보를 알고 싶습니다.',
+          answer: '',
+          answerDate: '',
         ),
         const SizedBox(height: 16.0),
         _buildAdRequestItem(
@@ -110,6 +113,9 @@ class AdListState extends State<AdList> with SingleTickerProviderStateMixin {
           date: '2024.09.07',
           status: '완료',
           isCompleted: true,
+          content: '광고 등록 절차를 알고 싶습니다.',
+          answer: '광고 등록 절차는 간단합니다. 먼저 양식을 작성해 주시면 됩니다.',
+          answerDate: '2024.09.08',
         ),
       ],
     );
@@ -139,7 +145,7 @@ class AdListState extends State<AdList> with SingleTickerProviderStateMixin {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => const AdDetail(
+                builder: (context) => const AdItemDetail(
                   title: '나의 히어로 아카데미아',
                   date: '2024.09.06',
                   content: '나의 히어로 아카데미아는 개꿀잼입니다!',
@@ -167,7 +173,7 @@ class AdListState extends State<AdList> with SingleTickerProviderStateMixin {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => const AdDetail(
+                builder: (context) => const AdItemDetail(
                   title: '원피스',
                   date: '2024.09.07',
                   content: '원피스도 개꿀잼이에요!',
@@ -186,73 +192,87 @@ class AdListState extends State<AdList> with SingleTickerProviderStateMixin {
     required String date,
     required String status,
     required bool isCompleted,
+    required String content,
+    required String answer,
+    required String answerDate,
   }) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AdRequestDetail(
+              title: title,
+              name: name,
+              date: date,
+              status: status,
+              isCompleted: isCompleted,
+              content: content,
+              answer: answer,
+              answerDate: answerDate,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
+                const SizedBox(height: 4.0),
+                Text(
+                  '$name | $date',
+                  style: const TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+              decoration: BoxDecoration(
+                color: isCompleted ? Colors.transparent : pinkColor,
+                borderRadius: BorderRadius.circular(12.0),
+                border: isCompleted ? Border.all(color: lightPinkColor) : null,
               ),
-              const SizedBox(height: 4.0),
-              Text(
-                name,
-                style: const TextStyle(
+              child: Text(
+                status,
+                style: TextStyle(
                   fontSize: 14.0,
-                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  color: isCompleted ? lightPinkColor : Colors.white,
                 ),
               ),
-              const SizedBox(height: 4.0),
-              Text(
-                date,
-                style: const TextStyle(
-                  fontSize: 12.0,
-                  color: Colors.grey,
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
-            decoration: BoxDecoration(
-              color: isCompleted ? Colors.transparent : pinkColor,
-              borderRadius: BorderRadius.circular(12.0),
-              border: isCompleted ? Border.all(color: lightPinkColor) : null,
             ),
-            child: Text(
-              status,
-              style: TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold,
-                color: isCompleted ? lightPinkColor : Colors.white,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-class AdDetail extends StatelessWidget {
+class AdItemDetail extends StatelessWidget {
   final String title;
   final String date;
   final String content;
 
-  const AdDetail({
+  const AdItemDetail({
     Key? key,
     required this.title,
     required this.date,
@@ -303,6 +323,131 @@ class AdDetail extends StatelessWidget {
               content,
               style: const TextStyle(fontSize: 16.0),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AdRequestDetail extends StatelessWidget {
+  final String title;
+  final String name;
+  final String date;
+  final String status;
+  final bool isCompleted;
+  final String content;
+  final String answer;
+  final String answerDate;
+
+  const AdRequestDetail({
+    Key? key,
+    required this.title,
+    required this.name,
+    required this.date,
+    required this.status,
+    required this.isCompleted,
+    required this.content,
+    required this.answer,
+    required this.answerDate,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const Color pinkColor = Color(0xFFE6A3B3);
+    const Color lightPinkColor = Color(0xFFF0B7C3);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '광고 요청 상세 정보',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '$name | $date',
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0, vertical: 6.0),
+                  decoration: BoxDecoration(
+                    color: isCompleted ? Colors.transparent : pinkColor,
+                    borderRadius: BorderRadius.circular(12.0),
+                    border:
+                        isCompleted ? Border.all(color: lightPinkColor) : null,
+                  ),
+                  child: Text(
+                    status,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: isCompleted ? lightPinkColor : Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              content,
+              style: const TextStyle(fontSize: 16.0),
+            ),
+            const SizedBox(height: 24.0),
+            if (isCompleted)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '광고 문의 답변드립니다.',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    '답변 날짜: $answerDate',
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    answer,
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
           ],
         ),
       ),
