602031199153092cdc3723a3d0a5189b0a082097
Promark1225 <wsw7854@naver.com>
2024-09-20 20:53:36 +0900

    fix: payment_history

--- lib/screen/user/payment_history.dart
+++ lib/screen/user/payment_history.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:pophub/model/pay_model.dart';
 import 'package:pophub/screen/custom/custom_title_bar.dart';
 
@@ -6,60 +8,57 @@ class PaymentHistoryPage extends StatefulWidget {
   const PaymentHistoryPage({super.key});
 
   @override
-  _PaymentHistoryPageState createState() => _PaymentHistoryPageState();
+  PaymentHistoryPageState createState() => PaymentHistoryPageState();
 }
 
-class _PaymentHistoryPageState extends State<PaymentHistoryPage> {
-  final List<Payment> payments = [
-    Payment(
-        date: '04.16',
-        description: '빵빵이 x 스미스 레더 키링',
-        amount: '-52,000원',
-        type: '구매'),
-    Payment(
-        date: '04.16',
-        description: '잇섭 맥세이프 충전기',
-        amount: '+89,000원',
-        type: '환불'),
-    Payment(
-        date: '04.12',
-        description: '플레이스테이션 세로 거...',
-        amount: '-42,000원',
-        type: '구매'),
-  ];
+class PaymentHistoryPageState extends State<PaymentHistoryPage> {
+  List<Payment> payments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPayments();
+  }
+
+  Future<void> fetchPayments() async {
+    final response =
+        await http.get(Uri.parse('https://3.88.120.90:3000/pay/search'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> paymentData = jsonDecode(response.body);
+      setState(() {
+        payments = paymentData.map((data) => Payment.fromJson(data)).toList();
+      });
+    } else {
+      throw Exception('결재 내역을 불러오지 못함');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomTitleBar(titleName: "결제 내역"),
-      body: ListView.builder(
-        itemCount: payments.length,
-        itemBuilder: (context, index) {
-          final payment = payments[index];
-          bool isNewDate =
-              index == 0 || payment.date != payments[index - 1].date;
-
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (isNewDate) ...[
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Text(payment.date,
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold)),
+      body: payments.isEmpty
+          ? const Center(
+              child: Text(
+                '결제 내역이 없습니다',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
-                const Divider(),
-              ],
-              ListTile(
-                title: Text(payment.description),
-                subtitle: Text(payment.type),
-                trailing: Text(payment.amount),
               ),
-            ],
-          );
-        },
-      ),
+            )
+          : ListView.builder(
+              itemCount: payments.length,
+              itemBuilder: (context, index) {
+                final payment = payments[index];
+                return ListTile(
+                  title: Text(payment.description),
+                  subtitle: Text(payment.date),
+                  trailing: Text(payment.amount),
+                );
+              },
+            ),
     );
   }
 }
