5f81cf20631601e00a2a074ad350c72915aa0f18
jiminh <rx460@naver.com>
2024-10-27 22:17:48 +0900

    - bugfix : 광고 overflow 오류 수정

--- lib/screen/adversiment/ad_edit.dart
+++ lib/screen/adversiment/ad_edit.dart
@@ -62,73 +62,75 @@ class _AdEditPageState extends State<AdEditPage> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.ad.title,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              '${widget.ad.startDate?.year}.${widget.ad.startDate?.month.toString().padLeft(2, '0')}.${widget.ad.startDate?.day.toString().padLeft(2, '0')}',
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 16),
-            if (widget.ad.img.isNotEmpty)
-              Image.network(
-                widget.ad.img,
-                width: 500,
-                height: 500,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  Logger.error(
-                      ('an_error_occurred_while_loading_image_error').tr());
-                  Logger.error(('stack_trace_stacktrace').tr());
-                  return Column(
-                    children: [
-                      Text(('the_image_cannot_be_loaded').tr()),
-                      const SizedBox(height: 8),
-                      Text(('error_error').tr(),
-                          style: const TextStyle(color: Colors.red)),
-                    ],
-                  );
-                },
-              )
-            else
-              Text(('there_are_no_images').tr()),
-            const Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: _registerAd,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: pinkColor,
-                  minimumSize: const Size(double.infinity, 50),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                ),
-                child: Text(
-                  ('advertisement_registration').tr(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.ad.title,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 8),
+              Text(
+                '${widget.ad.startDate?.year}.${widget.ad.startDate?.month.toString().padLeft(2, '0')}.${widget.ad.startDate?.day.toString().padLeft(2, '0')}',
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 16),
+              if (widget.ad.img.isNotEmpty)
+                Image.network(
+                  widget.ad.img,
+                  width: 500,
+                  height: 500,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    Logger.error(
+                        ('an_error_occurred_while_loading_image_error').tr());
+                    Logger.error(('stack_trace_stacktrace').tr());
+                    return Column(
+                      children: [
+                        Text(('the_image_cannot_be_loaded').tr()),
+                        const SizedBox(height: 8),
+                        Text(('error_error').tr(),
+                            style: const TextStyle(color: Colors.red)),
+                      ],
+                    );
+                  },
+                )
+              else
+                Text(('there_are_no_images').tr()),
+              const Spacer(),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _registerAd,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: pinkColor,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                  child: Text(
+                    ('advertisement_registration').tr(),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
