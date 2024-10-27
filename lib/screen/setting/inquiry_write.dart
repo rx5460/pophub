2f3d1f7f05a5d29dab29b1acc1ccbeeef80963ce
jiminh <rx460@naver.com>
2024-10-27 23:57:02 +0900

    - change  : 문의 수정

--- lib/screen/setting/inquiry_write.dart
+++ lib/screen/setting/inquiry_write.dart
@@ -104,157 +104,168 @@ class InquiryWritePageState extends State<InquiryWritePage> {
 
     return Scaffold(
       appBar: CustomTitleBar(titleName: ('titleName_12').tr()),
-      body: Padding(
-        padding: EdgeInsets.only(
-            left: screenWidth * 0.05,
-            right: screenWidth * 0.05,
-            top: screenHeight * 0.05,
-            bottom: screenHeight * 0.02),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              ('inquiry_category').tr(),
-              style:
-                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8.0),
-            DropdownButtonFormField<CategoryModel>(
-              decoration: InputDecoration(
-                labelText: ('labelText_8').tr(),
-                border: const OutlineInputBorder(),
-              ),
-              items: categoryList.map((categoryModel) {
-                return DropdownMenuItem<CategoryModel>(
-                  value: categoryModel,
-                  child: Text(categoryModel.categoryName),
-                );
-              }).toList(),
-              onChanged: (value) {
-                if (value != null) {
-                  setState(() {
-                    selectedCategory = value.categoryName;
-                  });
-                }
-              },
-            ),
-            const SizedBox(height: 16.0),
-            if (selectedCategory == ('advertisement_inquiry').tr())
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+              left: screenWidth * 0.05,
+              right: screenWidth * 0.05,
+              top: screenHeight * 0.05,
+              bottom: screenHeight * 0.02),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Text(
-                ('please_register_images_at_a_11_rationads_are_registered_in_popup_banner_formatnnwhen_registering_an_ad_modification_or_deletion_is_not_possible')
-                    .tr(),
+                ('inquiry_category').tr(),
                 style: const TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey,
-                ),
+                    fontSize: 16.0, fontWeight: FontWeight.bold),
               ),
-            const SizedBox(height: 16.0),
-            Text(
-              ('inquiry_subject').tr(),
-              style:
-                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8.0),
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(
-                hintText: ('hintText_11').tr(),
-                border: const OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Text(
-              ('inquiry_details').tr(),
-              style:
-                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8.0),
-            Expanded(
-              child: TextField(
-                controller: _contentController,
-                maxLines: null,
-                expands: true,
-                textAlignVertical: TextAlignVertical.top,
+              const SizedBox(height: 8.0),
+              DropdownButtonFormField<CategoryModel>(
                 decoration: InputDecoration(
-                  hintText: ('hintText_12').tr(),
+                  labelText: ('labelText_8').tr(),
+                  border: const OutlineInputBorder(),
+                ),
+                items: categoryList.map((categoryModel) {
+                  return DropdownMenuItem<CategoryModel>(
+                    value: categoryModel,
+                    child: Text(categoryModel.categoryName),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      selectedCategory = value.categoryName;
+                    });
+                  }
+                },
+              ),
+              const SizedBox(height: 16.0),
+              if (selectedCategory == ('advertisement_inquiry').tr())
+                Text(
+                  ('please_register_images_at_a_11_rationads_are_registered_in_popup_banner_formatnnwhen_registering_an_ad_modification_or_deletion_is_not_possible')
+                      .tr(),
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.grey,
+                  ),
+                ),
+              const SizedBox(height: 16.0),
+              Text(
+                ('inquiry_subject').tr(),
+                style: const TextStyle(
+                    fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8.0),
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  hintText: ('hintText_11').tr(),
                   border: const OutlineInputBorder(),
                 ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  ('attach_image').tr(),
-                  style: const TextStyle(
-                      fontSize: 16.0, fontWeight: FontWeight.bold),
+              const SizedBox(height: 16.0),
+              Text(
+                ('inquiry_details').tr(),
+                style: const TextStyle(
+                    fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8.0),
+              // 변경된 부분: Expanded를 SizedBox로 대체
+              SizedBox(
+                height: screenHeight * 0.4, // 원하는 높이로 설정
+                child: TextField(
+                  controller: _contentController,
+                  maxLines: null,
+                  expands: true,
+                  textAlignVertical: TextAlignVertical.top,
+                  decoration: InputDecoration(
+                    hintText: ('hintText_12').tr(),
+                    border: const OutlineInputBorder(),
+                  ),
                 ),
-                const SizedBox(height: 8.0),
-                _selectedImage == null
-                    ? OutlinedButton(
-                        onPressed: _pickImage,
-                        style: OutlinedButton.styleFrom(
-                          minimumSize: const Size(double.infinity, 50),
-                          backgroundColor: Colors.white,
-                          side: const BorderSide(color: Color(0xFFE6A3B3)),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              ('attach_1').tr(),
-                              style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(height: 16.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    ('attach_image').tr(),
+                    style: const TextStyle(
+                        fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8.0),
+                  _selectedImage == null
+                      ? OutlinedButton(
+                          onPressed: _pickImage,
+                          style: OutlinedButton.styleFrom(
+                            minimumSize: const Size(double.infinity, 50),
+                            backgroundColor: Colors.white,
+                            side: const BorderSide(color: Color(0xFFE6A3B3)),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
                             ),
-                            const Icon(
-                              Icons.arrow_forward_ios,
-                              color: Colors.black,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                ('attach_1').tr(),
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                              const Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.black,
+                              ),
+                            ],
+                          ),
+                        )
+                      : Column(
+                          children: [
+                            Image.file(
+                              _selectedImage!,
+                              height: 200,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(height: 8.0),
+                            OutlinedButton(
+                              onPressed: _pickImage,
+                              style: OutlinedButton.styleFrom(
+                                minimumSize: const Size(double.infinity, 50),
+                                backgroundColor: Colors.white,
+                                side:
+                                    const BorderSide(color: Color(0xFFE6A3B3)),
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                ),
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(10),
+                                    child: Text(
+                                      ('select_again').tr(),
+                                      style:
+                                          const TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                  const Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
-                      )
-                    : Column(
-                        children: [
-                          Image.file(
-                            _selectedImage!,
-                            height: 200,
-                            fit: BoxFit.cover,
-                          ),
-                          const SizedBox(height: 8.0),
-                          OutlinedButton(
-                            onPressed: _pickImage,
-                            style: OutlinedButton.styleFrom(
-                              minimumSize: const Size(double.infinity, 50),
-                              backgroundColor: Colors.white,
-                              side: const BorderSide(color: Color(0xFFE6A3B3)),
-                              shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                              ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  ('select_again').tr(),
-                                  style: const TextStyle(color: Colors.black),
-                                ),
-                                const Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.black,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                const SizedBox(height: 16.0),
-              ],
-            ),
-          ],
+                  const SizedBox(height: 16.0),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: SizedBox(
